419ce184a1c2111ad416d1c6ed705853bd4fe97c
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2021-08-20 01:28:27 -0400

    set const for process.env

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,6 +18,7 @@
  */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.4",
   defaultNetwork: "ropsten",
